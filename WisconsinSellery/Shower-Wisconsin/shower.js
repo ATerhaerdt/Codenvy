b1310ffad4bf7732b7b570162ffa3a81b68be8a1
ATerhaerdt <adam.terhaerdt@comcast.net>
2019-12-22 05:39:02 +0000

    Start of Sidebar and Google Sign In

--- WisconsinSellery/Shower-Wisconsin/shower.js
+++ WisconsinSellery/Shower-Wisconsin/shower.js
@@ -1,7 +1,24 @@
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 var other = $("#forNow");
+var a = $("#register");
+var b = $("sidebar1");
 
 hideElements(other);
+hideElements(a);
+
 
 function hideElements(element){
     $(element).hide();
 }
+
+
+// CLIENT ID: 261775333656-a251ptequpk7g8b25rmkr32tet8b3cf3.apps.googleusercontent.com
+// CLIENT SECRET: 6VuN14UjP9Ry3UxG5Y-sxxiW
