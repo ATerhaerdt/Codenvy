f6dcd2769596c96f05e5531e446e9921afc6c65f
ATerhaerdt <adam.terhaerdt@comcast.net>
2020-01-16 17:40:15 +0000

    Start of Transition

--- WisconsinSellery/Shower-Wisconsin/shower.js
+++ WisconsinSellery/Shower-Wisconsin/shower.js
@@ -12,6 +12,7 @@ var a = $("#signIn");
 var registry = $("#register")
 var contact = $("#contact")
 
+
 hideElements(other);
 hideElements(a);
 hideElements(contact);
@@ -26,7 +27,7 @@ function showElements(element){
     $(element).show();
 }
 
-
+/*
 $("#home1").click(function(){
     showElements(registry);
     hideElements(contact);
@@ -40,8 +41,28 @@ $("#contact1").click(function(){
     hideElements(registry);
     showElements(contact);
 });
+*/
 
 //<div class="g-signin2 googleMargin" data-onsuccess="onSignIn"></div>
 // CLIENT ID: 261775333656-a251ptequpk7g8b25rmkr32tet8b3cf3.apps.googleusercontent.com
 // CLIENT SECRET: 6VuN14UjP9Ry3UxG5Y-sxxiW
 
+
+
+setTimeout(transition, 1000);
+
+$('.js-trigger-transition').on('click', function(e) {
+  e.preventDefault();
+  transition();
+});
+
+function transition() {
+  var tl = new TimelineMax();
+  
+  tl.to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '50%' }, ease: Power2.easeOut}, 'close')
+  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '50%' }, ease: Power2.easeOut}, 'close')
+  .to($('.loader'), 0.2, {opacity: 1})
+  .to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '0%' }, ease: Power2.easeOut}, '+=1.5', 'open')
+  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '0%' }, ease: Power2.easeOut}, '-=0.2', 'open')
+  .to($('.loader'), 0.2, {opacity: 0}, '-=0.2');
+}
