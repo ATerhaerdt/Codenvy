8624e3cdd23cde89b47c50172282d5d3fc4b10fe
ATerhaerdt <adam.terhaerdt@comcast.net>
2020-01-26 19:02:07 +0000

    Transition + start of delay

--- WisconsinSellery/Shower-Wisconsin/shower.js
+++ WisconsinSellery/Shower-Wisconsin/shower.js
@@ -23,6 +23,36 @@ $("#sumbitB").click(function(){
 
 });
 
+
+$("#home1").click(function(){
+    transition($("#home1"));
+    delay(1000).showElements(registry);
+    delay(1000).hideElements(contact);
+    delay(1000).hideElements(other);
+});
+
+$("#info1").click(function(){
+    transition($("#info1"));
+    setTimeout(hideElements, 1500);
+        hideElements(contact);
+        hideElements(registry);
+    setTimeout(showElements, 1500);
+        showElements(other);
+});
+
+function transition(btn){
+    btn.disabled = true;
+    console.log(btn.disabled);
+    setTimeout(function() {
+        btn.disabled = false;
+        console.log(btn.disabled);
+    }, 3000);  
+    $("#bar1").animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+    $("#bar2").delay(200).animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+    $("#bar3").delay(400).animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+    $("#bar4").delay(600).animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+}
+
 /*
 $("#home1").click(function(){
     showElements(registry);
